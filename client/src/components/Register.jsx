f31d8aefb6b2f88bf7ef821bda94a7c0b15b0a61
Anderson Escorcia <andersondejesuseshe@ufps.edu.co>
2023-11-25 18:50:44 -0500

    correción color texto boton register

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -209,7 +209,7 @@ const togglePassword = () => {
                 onChange={uploadImage}/>
             </div>
           <div className="text-center mt-6">
-            <button className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150 text-black" type="submit"> Registrate </button>
+            <button className="bg-blueGray-800 text-black active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150 text-black" type="submit"> Registrate </button>
           </div>
         </form>
         {/* <div className="text-center">O registrate con:</div>
